7b4e3e031d4bd14c5fe7c5af9f2f2b2061fb645e
GNUxeava <adityakr082@gmail.com>
2020-10-09 22:11:53 +0530

    initial commit

---
+++ 4-13.c
@@ -0,0 +1,5 @@
+/* Collecting money becomes increasingly difficult during periods of recession,
+ * so companies may tighten their credit limits to prevent their accounts receivable
+ * (money owed to them) from becoming too large. In response to prolonged recession,
+ * one company has cut its customer's credit limits in half. Thus, if a customer had
+ * a credit limit of $2000, this customer's credit limit is now $1000 
