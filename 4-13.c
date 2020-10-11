97ed5c2593620e34e069a84004f9776b58af1253
GNUxeava <adityakr082@gmail.com>
2020-10-11 15:10:41 +0530

    finished stuff

--- 4-13.c
+++ 4-13.c
@@ -12,7 +12,7 @@
  * c) The customer's current balance (i.e., the amount the customer ows the company).
  *
  * Your program should calculate and print the new credit limit for each customer and 
- * shouod determine (and print) which customers have current balances that exceed 
+ * should determine (and print) which customers have current balances that exceed 
  * their new credit limits.
  */
 
@@ -22,11 +22,29 @@ int main()
 {
 	char accNo[11];
 	float cLimitBefore = 0.0;	/* credit limit before recession */
-	float currrentBal = 0.0;	/* current balance */
+	float currentBal = 0.0;	/* current balance */
 	float cLimitNew = 0.0;		/* new credit limit */
 
 	printf("Enter account number: ");
 	scanf("%s", &accNo);
 
-	printf("Enter credit limit before recessoion
+	printf("Enter credit limit before recessoion: ");
+	scanf("%f", &cLimitBefore);
+
+	printf("Enter customer's current balance: ");
+	scanf("%f", &currentBal);
+
+	cLimitNew = cLimitBefore / 2;
+
+	printf("\nAccount number: %s\n", accNo);
+	printf("Old credit limit: %f\n", cLimitBefore);
+	printf("New credit limit: %f\n", cLimitNew);
+	if (currentBal > cLimitNew)
+	{
+		printf("This customer's current balance exceeds their new credit limit.\n");
+	}
+
+	main();
+
+	return 0;
 }
