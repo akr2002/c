1c24edf5198249ca92f26878526f52d3fd7b7e1d
GNUxeava <adityakr082@gmail.com>
2020-10-21 11:20:43 +0530

    initial commit

---
+++ calc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2;
+	char op;
+
+	printf("Enter operator: ");
+	scanf("%c", &op);
+	printf("Enter two numbers\n");
+	scanf("%d%d", &num1, &num2);
+
+	switch(op)
+	{
+		case '+': printf("%d + %d = %d\n", num1, num2, num1 + num2);
+			  break;
+
+		case '-': printf("%d - %d = %d\n", num1, num2, num1 - num2);
+			  break;
+
+		case '*': printf("%d * %d = %d\n", num1, num2, num1 * num2);
+			  break;
+
+		case '/': if (!num2)
+			  {
+			  	printf("Cannot divide by 0\n");
+				return -1;
+			  }
+			  else
+			  {
+			  	printf("%d / %d = %d\n", num1, num2, num1 / num2);
+				break;
+			  }
+		default: printf("Invalid operator\n");
+			 return -1;
+	}
+
+	return 0;
+}
