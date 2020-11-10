d51e9156287b6588afb404939692f5048bbc0cdc
GNUxeava <adityakr082@gmail.com>
2020-11-10 21:16:39 +0530

    now takes command line arguments

--- ascii_num/ascii_num.c
+++ ascii_num/ascii_num.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "ascii_num.h"
 
-void print_num(int num)
+void print_num(char num)
 {
 	switch(num)
 	{
-		case 0: print_zero();
+		case '0': print_zero();
 			break;
 
-		case 1: print_one();
+		case '1': print_one();
 			break;
 
-		case 2: print_two();
+		case '2': print_two();
 			break;
 
-		case 3: print_three();
+		case '3': print_three();
 			break;
 			
-		case 4: print_four();
+		case '4': print_four();
 			break;
 
-		case 5: print_five();
+		case '5': print_five();
 			break;
 
-		case 6: print_six();
+		case '6': print_six();
 			break;
 
-		case 7: print_seven();
+		case '7': print_seven();
 			break;
 
-		case 8: print_eight();
+		case '8': print_eight();
 			break;
 
-		case 9: print_nine();
+		case '9': print_nine();
 			break;
 
 		default: break;
@@ -41,19 +42,31 @@ void print_num(int num)
 	return ;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	int num = -1;
-	while (1)
+	if (argc == 1)
 	{
-		printf("Enter an integer between 0 - 9 or press ^C to exit: ");
-		scanf("%d", &num);
-		if (num < 0 || num > 9)
+		printf("Please enter one or more arguments.\n");
+		return 0;
+	}
+
+	if (argc > 1)
+	{
+		int counter = 0;
+		for (counter = 1; counter < argc; ++counter)
 		{
-			continue;
+			int i = 0;
+			for (i = 0; argv[counter][i] != '\0'; ++i)
+			{
+				if (!(isdigit(argv[counter][i])))
+				{
+					continue;
+				}
+				else print_num(argv[counter][i]);
+			}
 		}
-		print_num(num);
 	}
 
 	return 0;
 }
+
